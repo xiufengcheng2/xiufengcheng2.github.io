0c77f2f88f69075f88a29b941277cf858db94601
Xat <i@xat.sh>
2025-01-13 16:16:42 +0800

    feat: adjust index page styles

--- src/components/ui/intro.tsx
+++ src/components/ui/intro.tsx
@@ -1,5 +1,4 @@
-import AvatarPng from "@/assets/image/avatar.png"
-import IntroPng from "@/assets/image/intro.png"
+import AvatarJpg from "@/assets/image/avatar.jpg"
 
 export function Intro({
 	description,
@@ -7,24 +6,21 @@ export function Intro({
 	status,
 }: IntroProps) {
 	return (
-		<section className="flex w-full items-center justify-center">
+		<section className="mt-24 flex w-full items-center justify-center gap-40">
 			<div className="flex flex-col justify-center">
-				<h1 className="mb-10 text-5xl font-bold text-theme">{name}</h1>
+				<h1 className="mb-14 text-5xl font-bold text-primary">{name}</h1>
 				<h2 className="mb-[30px] w-[512px] text-xl text-[#333333]">{status}</h2>
 				<p className="mb-[83px] w-[486px] text-[#727272]">{description}</p>
 				<a href="/about">
 					<button
-						className="w-50 rounded-full bg-primary px-6 py-2 text-white shadow-primary transition-shadow duration-300 hover:shadow-lg"
+						className="w-50 rounded-full bg-theme px-6 py-2 text-white shadow-theme transition-shadow duration-300 hover:shadow-lg"
 						type="button"
 					>
 						了解我更多…
 					</button>
 				</a>
 			</div>
-			<div className="relative">
-				<img alt="avatar" height={AvatarPng.height} src={AvatarPng.src} width={AvatarPng.width} />
-				<img alt="intro" className="absolute bottom-9 left-0 w-2/5" height={IntroPng.height} src={IntroPng.src} width={IntroPng.width} />
-			</div>
+			<img alt="avatar" className="h-[440px] w-[300px] rounded-[50%] object-cover object-top shadow-2xl" height={AvatarJpg.height} src={AvatarJpg.src} width={AvatarJpg.width} />
 		</section>
 	)
 }
